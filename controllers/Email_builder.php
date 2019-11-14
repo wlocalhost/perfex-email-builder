be9756e8f9f108457091a421f8d32c9ebc01f00a
Ion Prodan <ionprodan33@gmail.com>
2019-11-14 15:53:22 +0100

    angular no hash

--- controllers/Email_builder.php
+++ controllers/Email_builder.php
@@ -22,7 +22,7 @@ class Email_builder extends AdminController {
             access_denied('email_templates');
         }
         $data['title'] = _l('email_builder');
-        $data['css_files'] = glob(module_dir_path('email_builder', 'assets/perfex-email-builder/*.css'));
+        // $data['css_files'] = glob(module_dir_path('email_builder', 'assets/perfex-email-builder/*.css'));
         // $data['js_files'] = glob(module_dir_path('email_builder', 'assets/perfex-email-builder/*.js'));
         $this->load->view('email_builder', $data);
     }
--- views/email_builder.php
+++ views/email_builder.php
@@ -1,16 +1,14 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
-<?php foreach($css_files as $style) { ?>
-   <link rel="stylesheet" href="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/'.basename($style)) ?>">
-<?php } ?>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+<link rel="stylesheet" href="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/styles.css') ?>">
 <app-root id="wrapper"></app-root>
-<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/runtime-es2015.edb2fcf2778e7bf1d426.js') ?>" type="module"></script>
-<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/runtime-es5.edb2fcf2778e7bf1d426.js') ?>" nomodule defer></script>
-<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/polyfills-es5.fefa72e12b2a2d6ef7f2.js') ?>" nomodule defer></script>
-<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/polyfills-es2015.2987770fde9daa1d8a2e.js') ?>" type="module"></script>
-<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/main-es2015.7496a45fd79e0f903cc2.js') ?>" type="module"></script>
-<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/main-es5.7496a45fd79e0f903cc2.js') ?>" nomodule defer></script>
+<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/runtime-es2015.js') ?>" type="module"></script>
+<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/runtime-es5.js') ?>" nomodule defer></script>
+<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/polyfills-es5.js') ?>" nomodule defer></script>
+<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/polyfills-es2015.js') ?>" type="module"></script>
+<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/main-es2015.js') ?>" type="module"></script>
+<script src="<?php echo module_dir_url('email_builder', 'assets/perfex-email-builder/main-es5.js') ?>" nomodule defer></script>
 <?php init_tail(); ?>
 </body>
 </html>
