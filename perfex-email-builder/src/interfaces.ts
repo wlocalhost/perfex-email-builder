624c336f33068f648dc54f2bd28fc121f3b06816
Ion Prodan <ionprodan33@gmail.com>
2019-11-19 14:55:54 +0100

    Get and save email from DB

---
+++ perfex-email-builder/src/interfaces.ts
@@ -0,0 +1,14 @@
+export interface IPerfexEmail {
+  active: string;
+  emailtemplateid: string;
+  fromemail: string;
+  fromname: string;
+  language: string;
+  message: string;
+  name: string;
+  order: string;
+  plaintext: string;
+  slug: string;
+  subject: string;
+  type: string;
+}
